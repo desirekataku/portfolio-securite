f1ce4f13dbf6945938113fcdc518712159e284a2
desirekataku <desirekataku552@gmail.com>
2025-07-21 17:49:55 +0100

    Ajout du formulaire de contact2

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-
 document.getElementById('contact-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-  document.getElementById('response').innerText = "Merci pour votre message ! Je vous répondrai bientôt.";
+  e.preventDefault(); // Empêche le rechargement
+
+  const form = e.target;
+  const data = new FormData(form);
+
+  fetch(form.action, {
+    method: 'POST',
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      document.getElementById('response').innerText = "Merci pour votre message ! Je vous répondrai bientôt.";
+      form.reset(); // vide le formulaire
+    } else {
+      document.getElementById('response').innerText = "Oops, une erreur est survenue. Merci de réessayer.";
+    }
+  }).catch(() => {
+    document.getElementById('response').innerText = "Oops, une erreur est survenue. Merci de réessayer.";
+  });
 });
+
